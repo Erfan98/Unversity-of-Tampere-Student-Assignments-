57914b125a928bbd8eae9523cd2017c3d1152c80
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2021-03-01 19:19:50 +0600

    Almost Done week 7 Excercise!

---
+++ Ex7/app.js
@@ -0,0 +1,71 @@
+var datas={
+    "students": [
+      {
+        "id": 0,
+        "name": "Student A",
+        "course_assignment": null,
+        "exercise_points": 32
+      },
+      {
+        "id": 1,
+        "name": "Student B",
+        "course_assignment": null,
+        "exercise_points": 53
+      },
+      {
+        "id": 2,
+        "name": "Student C",
+        "course_assignment": null,
+        "exercise_points": null
+      },
+      {
+        "id": 3,
+        "name": "Student D",
+        "course_assignment": null,
+        "exercise_points": 4
+      }
+    ]
+  }
+
+
+
+var url = 'http://aws.random.cat/meow';
+fetch(url)
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(data){
+        document.getElementById('cat').innerHTML=data.file
+        document.getElementById('cat').setAttribute("href",data.file)
+    });
+
+function gentable(){
+    var t= document.getElementById('table')
+    t.innerHTML='';
+    var x=''
+    console.log(datas.students[2].name)
+    len=datas.students.length
+    for(var i=0; i<len;i++){
+        
+        var row=`<tr>
+                    <td>${datas.students[i].name}</td>
+                    <td>${datas.students[i].course_assignment}</td>
+                    <td>${datas.students[i].exercise_points}</td>
+                </tr>`
+        t.innerHTML += row;
+
+    }
+}
+
+
+
+function add(){
+    n=document.getElementById('name').value;
+    info=document.getElementById('info').value;
+    points=document.getElementById('points').value;
+    console.log(n,info,points)
+    datas.students.push({id: 4, "name":n,"course_assignment":info,"exercise_points": points});
+    gentable();
+    console.log(datas)
+
+}
